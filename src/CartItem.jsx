4a177faeaf4273407de7d24f510e578457cfb2fb
WeyinmiOProgrammer <weyinmi05@gmail.com>
2025-10-27 12:00:30 -0400

    partially added cartitems code

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,51 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;   
+    cart.forEach((item) => {
+        if (item.quantity > 0)
+        {
+           totalAmount += parseFloat(item.cost.substring(1)) * item.quantity;
+        }
+        
+       });
+    return totalAmount;   
   };
 
   const handleContinueShopping = (e) => {
-   
+      onContinueShopping(e);
   };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  }
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity+1}))
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1)
+    {
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity-1}))
+    }
+    else
+    {
+        dispatch(removeItem({name: item.name}))
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({name: item.name}))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+     let totalCost = parseFloat(item.cost.substring(1)) * item.quantity;
+     return totalCost;
+
   };
 
   return (
